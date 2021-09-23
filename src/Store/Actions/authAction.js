6f7db55134be0febe97ea1d997a7bded60faf77e
Claudia Angelina <claudiaangelina@Claudias-Air.domain.name>
2021-09-24 00:20:23 +0700

    forbidden page

--- src/Store/Actions/authAction.js
+++ src/Store/Actions/authAction.js
@@ -1,14 +1,7 @@
 import axios from "axios";
 
-export function doLogin(params) {
+export function doLogin(data) {
   return async (dispatch) => {
-    try {
-      dispatch({ type: "LOGIN", payload: { isLoading: true } });
-      const { data } = await axios.post(
-        `http://localhost:5002/users/login`,
-        params
-      );
-
       dispatch({
         type: "LOGIN",
         payload: {
@@ -17,53 +10,31 @@ export function doLogin(params) {
           isLoading: false,
         },
       });
-      localStorage.setItem("access_token", data.token);
-    } catch (err) {
-      console.log(err);
-    }
   };
 }
 
-export function doRegister(data) {
-  return (dispatch) => {
-    axios
-      .post(`http://localhost:5002/users/register`, data)
-      .then((res) => {
-        console.log("register:", res);
-        dispatch({
-          type: "LOGIN",
-          payload: {
-            ...res.data,
-            isLogin: true,
-          },
-        });
-      })
-      .catch((err) => console.log(err));
-  };
+export function doLogout() {
+  return async (dispatch) => {
+    await localStorage.removeItem('userId')
+    const userId = await localStorage.getItem('userId');
+    console.log("logout:", userId);
+    dispatch({
+      type: 'LOGOUT'
+    })
+  }
 }
 
-// export function doLogout() {
-//   return async (dispatch) => {
-//     await localStorage.removeItem('userId')
-//     const userId = await localStorage.getItem('userId');
-//     console.log("logout:", userId);
-//     dispatch({
-//       type: 'LOGOUT'
-//     })
-//   }
-// }
-
-// export function checkLogin() {
-//   return async dispatch => {
-//     const userId = await localStorage.getItem('userId');
-//     console.log("[checkLogin] userId:", userId);
-//     if (userId) {
-//       const { data } = await axios.get(`${url}/users?id=${userId}`);
-//       // console.log("[checkLogin] data:", data);
-//       dispatch({
-//         type: 'LOGIN',
-//         payload: { ...data[0], isLogin: true, isLoading: false }
-//       });
-//     };
-//   };
-// }
+export function checkLogin() {
+  return async dispatch => {
+    const token = await localStorage.getItem('access_token');
+    console.log("[checkLogin] token:", token);
+    if (token) {
+      const { data } = await axios.get(`http://localhost:5001/users/check-token`, {headers : { "Authorization" : token }});
+      console.log("[checkLogin] data:", data);
+      dispatch({
+        type: 'LOGIN',
+        payload: { ...data, isLogin: true, isLoading: false }
+      });
+    };
+  };
+}
--- src/Store/Reducers/authReducer.js
+++ src/Store/Reducers/authReducer.js
@@ -1,22 +1,24 @@
 const initialState = {
-    id: '',
-    email: '',
-    isLogin: false,
+  id: '',
+  nama : '',
+  role: '',
+  isLogin: false,
+}
+
+export default function authReducer(state = initialState, action) {
+  switch(action.type) {
+    case 'LOGIN':
+      // console.log("action payload:", action.payload)
+      return {
+        ...state,
+        id: action.payload.id,
+        nama: action.payload.nama,
+        role: action.payload.role,
+        isLogin: action.payload.isLogin,
+      };
+    case 'LOGOUT':
+      return initialState;
+    default:
+      return state;
   }
-  
-  export default function authReducer(state = initialState, action) {
-    switch(action.type) {
-      case 'LOGIN':
-        // console.log("action payload:", action.payload)
-        return {
-          ...state,
-          id: action.payload.id,
-          email: action.payload.email,
-          isLogin: action.payload.isLogin,
-        };
-      case 'LOGOUT':
-        return initialState;
-      default:
-        return state;
-    }
-  }
+}
